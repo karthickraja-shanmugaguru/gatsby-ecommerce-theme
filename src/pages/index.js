fb013dcc28a2767873d4833e4fdd3f11b54b296d
karthickraja-shanmujgaguru <karthickraja2k2@gmail.com>
2024-09-18 20:58:38 +0530

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Say'}
+        subtitle={'Zero junk'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
